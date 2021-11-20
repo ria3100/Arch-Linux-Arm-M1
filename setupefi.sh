bcde5d557fffeb469c9bcdf353967422a1fe1729
Ria <ria000130@gmail.com>
2021-11-20 23:37:14 +0900

    feat: add cloud-init

--- setupefi.sh
+++ setupefi.sh
@@ -7,4 +7,17 @@ pacman-key --populate archlinuxarm
 pacman -Syu --noconfirm efibootmgr
 efibootmgr --disk /dev/vda --part 1 --create --label "Arch Linux ARM" --loader /Image --unicode "root=UUID=$EXT4_UUID rw initrd=\initramfs-linux.img" --verbose
 rm $0
+
+pacman -S --noconfirm python-pip
+pacman -S --noconfirm cloud-guest-utils
+pacman -S --noconfirm wget
+
+wget https://launchpad.net/cloud-init/trunk/17.1/+download/cloud-init-17.1.tar.gz
+tar -zxvf cloud-init-17.1.tar.gz
+cd cloud-init-17.1
+pip install -r requirements.txt
+
+python setup.py build
+python setup.py install --init-system systemd
+
 poweroff
