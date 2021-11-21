0464078abcb1b50a319902535d7604445c62d189
Ria <ria000130@gmail.com>
2021-11-21 14:53:08 +0900

    fix: copy util.py

--- setupefi.sh
+++ setupefi.sh
@@ -19,8 +19,9 @@ tar -zxvf cloud-init-17.1.tar.gz
 cd cloud-init-17.1
 pip install -r requirements.txt
 
-wget https://gist.githubusercontent.com/ria3100/a6dc3fcddd39e02727e55f92bf489b27/raw/d842db93ff27a7f28b92cc765d82b9965b8204ae/cloud-init-util.py -O util.py
 python setup.py build
+wget https://gist.githubusercontent.com/ria3100/a6dc3fcddd39e02727e55f92bf489b27/raw/d842db93ff27a7f28b92cc765d82b9965b8204ae/cloud-init-util.py
+mv cloud-init-util.py cloudinit/util.py
 python setup.py install --init-system systemd
 
 poweroff
