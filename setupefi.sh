4c98e9effecb7afec0c9dfd9008d86e4090db863
Ria <ria000130@gmail.com>
2021-11-21 14:16:08 +0900

    fix: replace util.py

--- setupefi.sh
+++ setupefi.sh
@@ -9,20 +9,17 @@ efibootmgr --disk /dev/vda --part 1 --create --label "Arch Linux ARM" --loader /
 rm $0
 
 pacman -Syu
-pacman -S --noconfirm gcc
-pacman -S --noconfirm pyenv
 
+pacman -S --noconfirm python-pip
 pacman -S --noconfirm cloud-guest-utils
 pacman -S --noconfirm wget
-
-pyenv install 3.7.12
-pyenv global 3.7.12
 
 wget https://launchpad.net/cloud-init/trunk/17.1/+download/cloud-init-17.1.tar.gz
 tar -zxvf cloud-init-17.1.tar.gz
 cd cloud-init-17.1
 pip install -r requirements.txt
 
+wget https://gist.githubusercontent.com/ria3100/a6dc3fcddd39e02727e55f92bf489b27/raw/d842db93ff27a7f28b92cc765d82b9965b8204ae/cloud-init-util.py -O util.py
 python setup.py build
 python setup.py install --init-system systemd
 
